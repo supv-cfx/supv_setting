460d4e30760bc35fb83b9c8031db74f908e174b9
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-23 15:43:54 +0000

    add: fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games {'gta5', 'rdr3'}
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+version '1.0'
+
+author 'SUP2Ak#3755'
+link 'https://github.com/SUP2Ak/supv_setting'
+github 'https://github.com/SUP2Ak'
+
+
+shared_script '@supv_core/obj.lua'
+
+files {
+    'modules/client/*.lua',
+    'config/client/*.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    'moduleHandlers.lua'
+}
