33f039c38f3c3ec8f8ae564e9decd763ccd0f05a
SUP2Ak <cormier.wesley@gmail.com>
2023-07-28 21:35:12 +0200

    refactor(client\dispatch): ManageDispatch

--- modules/client/dispatch.lua
+++ modules/client/dispatch.lua
@@ -1,13 +1,25 @@
 local EnableDispatchService <const> = EnableDispatchService
 local BlockDispatchServiceResourceCreation <const> = BlockDispatchServiceResourceCreation
 
+---@param value boolean
+---@param index integer
+local function ManageDispatch(value, index)
+    EnableDispatchService(value, index)
+    BlockDispatchServiceResourceCreation(index, not value)
+end
+
 return function(config)
     supv.onCache('ped', function(value)
         for i = 0, #config do
             local v = config[i]
-            EnableDispatchService(v, i)
-            v = not v
-            BlockDispatchServiceResourceCreation(i, v)
+            ManageDispatch(v, i)
+        end
+    end)
+
+    CreateThread(function()
+        for i = 0, #config do
+            local v = config[i]
+            ManageDispatch(v, i)
         end
     end)
 end
