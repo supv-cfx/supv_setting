543cbde6db5fb9c99664123f65228fa8b455d27e
SUP2Ak <cormier.wesley@gmail.com>
2023-07-28 20:37:11 +0200

    feat(moduleHandlers); load resource manager

--- moduleHandlers.lua
+++ moduleHandlers.lua
@@ -13,18 +13,29 @@ if supv.service == 'client' then
     end
 elseif supv.service == 'server' then
     local config = require 'config'
-
-    if config.rm then
-        require 'server.resource_manager'(config.client, config.server)
-    end
-
+    local data, c = config.rm and {server = {}, client = {}}
     for k, v in pairs(config.server) do
         if v == true then
             local modname = ('modules.%s.%s'):format(supv.service, k)
             local module = require(modname)
             if type(module) == 'function' then
-                module(supv.json.load(('data.%s.%s'):format(supv.service, k)))
+                c = supv.json.load(('data.%s.%s'):format(supv.service, k))
+                module(c)
+                if data and not data.server[k] then
+                    data.server[k] = c
+                end
             end
         end
     end
+
+    if not data then return end
+
+    for k,v in pairs(config.client) do
+        c = supv.json.load(('data.%s.%s'):format('client', k))
+        if not data.client[k] then
+            data.client[k] = c
+        end
+    end
+
+    require 'modules.server.resource_manager'(data.client, data.server, config)
 end
